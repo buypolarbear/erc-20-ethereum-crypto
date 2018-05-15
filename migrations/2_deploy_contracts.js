d917d08765dd016028eb1dd3d20069caae62151c
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-15 03:43:43 -0400

    [TASK] Updated migrations to support constructor argument

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
-var CoffeeTokenMigrations = artifacts.require("./CoffeeToken.sol");
+var CoffeeToken = artifacts.require("./CoffeeToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(CoffeeTokenMigrations);
+  // set initial supply to 1 million
+  deployer.deploy(CoffeeToken, 1000000);
 };
