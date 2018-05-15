93f8013a1671242e8c55b18e54b6a140c88844af
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-15 03:43:21 -0400

    [WIP] Added tests for name and symbol

--- test/CoffeeToken.js
+++ test/CoffeeToken.js
@@ -1,11 +1,30 @@
 let CoffeeToken = artifacts.require("./CoffeeToken");
 
-contract("CoffeeToken", function(accounts){
+contract("CoffeeToken", (accounts) => {
 
+    let tokenInstance = '';
+    let totalSupply = '';
+    beforeEach(async () => {
+        tokenInstance = await CoffeeToken.deployed();
+        totalSupply = await tokenInstance.totalSupply();
+    });
+
+    // check for total supply on deploy
     it("sets the total supply upon deployment", async () => {
-        let tokenInstance = await CoffeeToken.deployed();
-        let totalSupply = await tokenInstance.totalSupply();
 
         assert.equal(totalSupply.toNumber(), 1000000);
-    })
+
+        let adminBalance = await tokenInstance.balanceOf(accounts[0]);
+
+        assert.equal(adminBalance.toNumber(), 1000000);
+    });
+
+    // check for symbol and name
+    it("initializes the contract with correct values", async () => {
+        let name = tokenInstance.name();
+        let symbol = tokenInstance.symbol();
+
+        assert.equal(name, 'Coffee Token');
+        assert.equal(symbol, 'CFFT');
+    });
 });
